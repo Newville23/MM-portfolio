051c8d758c39c42c81db160cf05ea178a3948248
David Villanueva <david.villanueva.dev@gmail.com>
2025-02-01 16:47:21 -0500

    [mm-01]: updates nav and global style

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,42 @@
-import Link from 'next/link'
-
+import Link from "next/link";
+import { heatherFont } from "app/lib/fonts";
 const navItems = {
-  '/': {
-    name: 'home',
+  "/": {
+    name: "work",
   },
-  '/blog': {
-    name: 'blog',
+  "/blog": {
+    name: "contact",
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
-}
+};
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[8px] mb-16">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row justify-between relative px-0 pb-0 fade md:overflow-hidden scroll-pr-6 md:relative"
           id="nav"
         >
+          <div>
+            <Link href={"/"} className={`transition-all uppercase text-5xl ${heatherFont.className}`}>
+              M
+            </Link>
+          </div>
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 uppercase tracking-[0.2rem] text-sm flex align-middle relative py-1 px-2 first:mr-14 font-extrabold"
                 >
                   {name}
                 </Link>
-              )
+              );
             })}
           </div>
         </nav>
       </div>
     </aside>
-  )
+  );
 }
